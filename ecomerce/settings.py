6724c42621d8f5851ed40727bea81932b5c5e510
sulaimonazeez <olaniyisulaimon221@gmail.com>
2023-10-18 08:34:51 +0100

    commited

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xyz9^k+%u329n_ruq)n3os0k-vu28oz7p_su0r1ha@t6ne$#+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
