2ade56b2b6c2f3b52fbd1b030def8253e5994941
Azeez Sulaimon <111203778+sulaimonazeez@users.noreply.github.com>
2025-02-14 17:35:20 -0600

    updated

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xyz9^k+%u329n_ruq)n3os0k-vu28oz7p_su0r1ha@t6ne$#+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "myapp",
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
